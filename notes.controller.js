c34f80bf030f902bb043fcc6cb1fadb4d917fa50
Alexander M <alexander2555@yandex.ru>
2025-09-30 12:30:00 +0200

    feat: add Remove operation | modify List output

--- notes.controller.js
+++ notes.controller.js
@@ -4,7 +4,7 @@ const path = require('path')
 const notesPath = path.join(__dirname, 'db.json')
 
 async function addNote(title) {
-  const notes = await getNotes() // Buffer.from(buffer).toString(('utf-8')) // require('./db.json')
+  const notes = await getNotes() // Buffer.from(buffer).toString(('utf-8'))// require('./db.json')
 
   const note = {
     title,
@@ -21,7 +21,17 @@ async function getNotes() {
   return Array.isArray(notes) ? notes : []
 }
 
+async function removeNote(id) {
+  const notes = await getNotes()
+
+  await fs.writeFile(
+    notesPath,
+    JSON.stringify(notes.filter((n) => n.id !== id))
+  )
+}
+
 module.exports = {
   addNote,
   getNotes,
+  removeNote,
 }
